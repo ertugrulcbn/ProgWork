72105f4fd9eb6aa84dbec5080ff2752ca1460427
Unknown <ertugrulcbn@gmail.com>
2018-10-24 16:12:46 +0300

    CW4 Fix

--- Database.js
+++ Database.js
@@ -1,16 +1,5 @@
-class Course{
-    constructor(name="",time="",date="",rooms=[]){
-        this.name=name;
-        this.time=time;
-        this.date=date;
-        this.rooms=rooms;
-    }
-    toString(){
-      return this.name;
-    }
-}
 class Student{
-    constructor(id=0,name="",gpa=0,courses=[]){
+    constructor(id=10,name="empty",gpa=0,courses=[]){
         this.id=id;
         this.name=name;
         this.gpa=gpa;
@@ -20,3 +9,15 @@ class Student{
       return this.id+"";
     }
 }
+class Course{
+    constructor(name="empty",time="empty",date="empty",rooms=[]){
+        this.name=name;
+        this.time=time;
+        this.date=date;
+        this.rooms=rooms;
+    }
+    toString(){
+      return this.name;
+    }
+}
+
