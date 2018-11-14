7da4ec4f9827f42812627aab1c19a446a224f873
ertugrulcbn <ertugrulcbn@gmail.com>
2018-11-14 16:23:29 +0300

    Add files via upload

--- Database.js
+++ Database.js
@@ -1,23 +1,33 @@
-class Student{
-    constructor(id=10,name="empty",gpa=0,courses=[]){
-        this.id=id;
-        this.name=name;
-        this.gpa=gpa;
-        this.courses=courses;
-    }
-    toString(){
-      return this.id+"";
-    }
+"use strict";
+class Course {
+  constructor(name, time, date) {
+    this.name = name
+    this.time = time
+    this.date = date
+    this.rooms = []
+  }
+  addRooms(...room) {
+    for (let r of room) this.rooms.push(r)
+    return this
+  }
+  toString() {
+    return this.name;
+  }
 }
-class Course{
-    constructor(name="empty",time="empty",date="empty",rooms=[]){
-        this.name=name;
-        this.time=time;
-        this.date=date;
-        this.rooms=rooms;
-    }
-    toString(){
-      return this.name;
-    }
+
+class Student {
+  constructor(id, name, gpa) {
+    this.id = id
+    this.name = name
+    this.gpa = gpa
+    this.courses = []
+  }
+  addCourses(...course) {
+    for (let c of course) this.courses.push(c)
+    return this
+  }
+  toString() {
+    return this.name +": "+this.gpa;
+  }
 }
 
